45382994b6aaf5bcefb8839632ec574999aeb8d7
Willy Kloucek <wkloucek@owncloud.com>
2021-05-12 08:17:31 +0200

    adapt to proper license handling

--- data/shared/owncloud.sh
+++ data/shared/owncloud.sh
@@ -6,7 +6,7 @@ CONTAINER_STATION_DIR=$(/sbin/getcfg container-station Install_Path -f $QPKG_CON
 QPKG_PROXY_FILE=/etc/container-proxy.d/$QPKG_NAME
 OWNCLOUD_ROOT=$(/sbin/getcfg $QPKG_NAME Install_Path -f $QPKG_CONF)
 
-CRON_JOB="*/5 * * * *  $OWNCLOUD_ROOT/owncloud.sh get-licenses"
+CRON_JOB="*/5 * * * *  $OWNCLOUD_ROOT/owncloud.sh license-handler"
 CRON_FILE=/etc/config/crontab
 
 cd $OWNCLOUD_ROOT
@@ -15,7 +15,7 @@ _load_images() {
     DOCKER_IMAGES_SOURCE=$OWNCLOUD_ROOT/docker-images/DOCKER_IMAGES
     if test -f "$DOCKER_IMAGES_SOURCE"; then
         DOCKER_IMAGES=$(cat $DOCKER_IMAGES_SOURCE)
-        
+
         for docker_image in $DOCKER_IMAGES; do
             $CONTAINER_STATION_DIR/bin/system-docker inspect $docker_image
             exit_status=$?
@@ -52,94 +52,103 @@ _register_license() {
 case "$1" in
     start)
         _register_license
-        $0 get-licenses
         _load_images
-        
+
         $CONTAINER_STATION_DIR/bin/system-docker-compose up -d --remove-orphans
-        
+
         grep -qF "$CRON_JOB" "$CRON_FILE"  || echo "$CRON_JOB" | tee -a "$CRON_FILE"
         crontab $CRON_FILE
-        
+
         _proxy_start
         _proxy_reload
     ;;
-    
+
     stop)
         $CONTAINER_STATION_DIR/bin/system-docker-compose down --remove-orphans
-        
+
         _proxy_stop
         _proxy_reload
     ;;
-    
+
     restart)
         $0 stop
         $0 start
     ;;
-    
+
     remove)
         ESCAPED_CRON_JOB=$(printf '%s\n' "$CRON_JOB" | sed -e 's/[]\/$*.^[]/\\&/g');
         sed -i "/$ESCAPED_CRON_JOB/d" $CRON_FILE
         crontab $CRON_FILE
         $0 stop
     ;;
-    
-    get-licenses)
-        mkdir -p $OWNCLOUD_ROOT/licenses
-        qlicense_tool installed_list -n -d -a $QPKG_NAME > $OWNCLOUD_ROOT/licenses/owncloud.json
+
+    license-handler)
+        licenses=$(qlicense_tool installed_list -a $QPKG_NAME)
+        count=$(echo $licenses | jq -r '.result' | jq length)
+
+        if [ "$count" -eq "0" ]; then
+            $CONTAINER_STATION_DIR/bin/system-docker-compose exec owncloud occ config:app:delete enterprise_key license-key
+        else
+            $CONTAINER_STATION_DIR/bin/system-docker-compose exec owncloud occ config:app:set --value "'"$licenses"'" enterprise_key license-key
+        fi
     ;;
-    
+
     callback_success)
         echo '{"code": "0", "message": "", "info": "", "result": []}'
     ;;
-    
+
     before_buy)
         $0 callback_success
     ;;
-    
+
     before_buy_variant)
         $0 callback_success
     ;;
-    
+
     before_activate)
         $0 callback_success
     ;;
-    
+
     before_deactivate)
         $0 callback_success
     ;;
-    
+
     after_activate)
+        $0 license-handler
         $0 callback_success
     ;;
-    
+
     after_deactivate)
+        $0 license-handler
         $0 callback_success
     ;;
-    
+
     after_expired)
+        $0 license-handler
         $0 callback_success
     ;;
-    
+
     after_extend)
+        $0 license-handler
         $0 callback_success
     ;;
-    
+
     debug-container-status)
         $CONTAINER_STATION_DIR/bin/system-docker-compose ps
     ;;
-    
+
     debug-owncloud-logs)
         $CONTAINER_STATION_DIR/bin/system-docker-compose logs -f owncloud
     ;;
-    
+
     debug-db-logs)
         $CONTAINER_STATION_DIR/bin/system-docker-compose logs -f db
     ;;
-    
+
     debug-redis-logs)
         $CONTAINER_STATION_DIR/bin/system-docker-compose logs -f redis
     ;;
-    
+
     *)
         echo "Usage: $0 {start|stop|restart}"
         exit 1
