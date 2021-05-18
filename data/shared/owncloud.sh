25d3a4715ea1fe074f1c41260c326ebd763b9d26
Willy Kloucek <wkloucek@owncloud.com>
2021-05-18 11:39:52 +0200

    only pass results and base64 encode it

--- data/shared/owncloud.sh
+++ data/shared/owncloud.sh
@@ -83,13 +83,14 @@ case "$1" in
     ;;
 
     license-handler)
-        licenses=$(qlicense_tool installed_list -a $QPKG_NAME)
-        count=$(echo $licenses | jq -r '.result' | jq length)
+        qlicense="$(qlicense_tool installed_list -a $QPKG_NAME)"
+        count="$(echo $qlicense | jq -r '.result' | jq length)"
 
         if [ "$count" -eq "0" ]; then
             $CONTAINER_STATION_DIR/bin/system-docker-compose exec owncloud occ config:app:delete enterprise_key license-key
         else
-            $CONTAINER_STATION_DIR/bin/system-docker-compose exec owncloud occ config:app:set --value "'"$licenses"'" enterprise_key license-key
+            licenses="$(echo $qlicense | jq -r '.result' | jq -c -M . | base64)"
+            $CONTAINER_STATION_DIR/bin/system-docker-compose exec owncloud occ config:app:set --value "${licenses//[^[:alnum:]]/}" enterprise_key license-key
         fi
     ;;
 
